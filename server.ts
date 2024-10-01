f817ea441f80edc27ba1ba36046978d2a79e128d
Your_Name <Your_Email>
2024-10-01 16:47:04 +0530

    setup express server

--- server.ts
+++ server.ts
@@ -1,1 +1,11 @@
-console.log("server start");
+import app from "./src/app";
+
+function startServer() {
+    const port = process.env.PORT || 8000;
+
+    app.listen(port, () => {
+        console.log(`Server start at ${port}`);
+    });
+}
+
+startServer();
