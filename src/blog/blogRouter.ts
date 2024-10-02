e62a6b582c3b1622add99c21a82b88e793be8e3f
Your_Name <Your_Email>
2024-10-02 23:10:31 +0530

    multer setup

--- src/blog/blogRouter.ts
+++ src/blog/blogRouter.ts
@@ -1,9 +1,15 @@
 import express from "express";
 import { createBlogPost } from "./blogController";
+import multer from "multer";
+import path from "node:path";
 
 const blogRouter = express.Router();
 
+const upload = multer({
+    dest: path.resolve(__dirname, "../../public/data/uploads"),
+    limits: { fileSize: 3e7 }, //3e7 -> 30 MB
+});
 //routes
-blogRouter.post("/", createBlogPost);
+blogRouter.post("/", upload.single("coverImage"), createBlogPost);
 
 export default blogRouter;
