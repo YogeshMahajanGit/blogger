ce5a9feab6f1c55dd9e0113c41b4e58157e7fe55
Your_Name <Your_Email>
2024-10-03 20:58:49 +0530

    create blog post

--- src/blog/blogController.ts
+++ src/blog/blogController.ts
@@ -1,6 +1,8 @@
 import { Request, Response, NextFunction } from "express";
 import { S3, PutObjectCommand, ObjectCannedACL } from "@aws-sdk/client-s3";
 import { config } from "../config/config";
+import createHttpError from "http-errors";
+import BlogPost from "./blogModel";
 
 // Initialize S3 client
 const s3 = new S3({
@@ -16,14 +18,13 @@ async function createBlogPost(
     res: Response,
     next: NextFunction
 ): Promise<void> {
-    console.log("files : ", req.file);
+    const { title, content, blogger, category, comments } = req.body;
 
     // Check if file is present
     if (!req.file) {
         res.status(400).json({ error: "No image file uploaded" });
         return;
     }
-
     // Upload Function to S3
     const uploadToS3 = async (file: Express.Multer.File) => {
         const key = `${Date.now()}_${file.originalname}`;
@@ -40,28 +41,39 @@ async function createBlogPost(
     };
 
     try {
-        // Upload the image to S3
+        // Upload the image to AWS S3
         const key = await uploadToS3(req.file);
-
         // Construct the image URL
         const imageUrl = `https://my-blogger-images.s3.${config.region}.amazonaws.com/${key}`;
 
-        console.log("IMG URL: ", imageUrl);
+        //Create New Blog Post
+        const newBlogPost = await BlogPost.create({
+            //66fcca09f7aa79280f508bc3
+            title,
+            content,
+            category,
+            blogger: "66fcca09f7aa79280f508bc3",
+            coverImage: imageUrl,
+            comments,
+        });
+
         res.status(201).json({
-            message: "Image uploaded successfully",
-            imageUrl: imageUrl,
+            message: "Post Created Successfully",
+            id: newBlogPost._id,
         });
     } catch (error) {
-        console.error("Error uploading image:", error);
-        if (error instanceof Error) {
-            res.status(500).json({
-                error: `Failed to upload image: ${error.message}`,
-            });
-        } else {
-            res.status(500).json({
-                error: "Failed to upload image due to an unknown error",
-            });
-        }
+        console.error("Error creating blog post:", error);
+        return next(createHttpError(500, "Error while creating blog"));
+
+        // if (error instanceof Error) {
+        //     res.status(500).json({
+        //         error: `Failed to upload image: ${error.message}`,
+        //     });
+        // } else {
+        //     res.status(500).json({
+        //         error: "Failed to upload image due to an unknown error",
+        //     });
+        // }
     }
 }
 
